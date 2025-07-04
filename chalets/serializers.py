da8ae43b111619f0f4f58f2c5fb4e960b21bc9e7
mohamedmostafa33 <mohamedabohamama15@gmail.com>
2025-07-04 19:00:56 +0200

    Add ChaletSerializer to handle Chalet model serialization

---
+++ chalets/serializers.py
@@ -0,0 +1,8 @@
+from django import serializers
+from .models import Chalet
+
+class ChaletSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chalet
+        fields = ['id', 'name', 'number_of_rooms', 'price_per_night', 'notes', 'created_at']
+        read_only_fields = ['id', 'created_at']
