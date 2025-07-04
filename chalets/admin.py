e2d550a9b37fb00241040289f403ebb65cf9306e
mohamedmostafa33 <mohamedabohamama15@gmail.com>
2025-07-04 18:49:46 +0200

    Register Chalet model in Django Admin with list display, search, and filter options

--- chalets/admin.py
+++ chalets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Chalet
 
-# Register your models here.
+@admin.register(Chalet)
+class ChaletAdmin(admin.ModelAdmin):
+    list_display = ('name', 'number_of_rooms', 'price_per_night', 'created_at')
+    search_fields = ('name',)
+    list_filter = ('number_of_rooms', 'created_at')
