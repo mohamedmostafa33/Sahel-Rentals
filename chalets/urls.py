5b059a0808524c1471689f9b38f6838119293f38
mohamedmostafa33 <mohamedabohamama15@gmail.com>
2025-07-05 04:49:06 +0200

    Add ChaletUpdateView endpoint to enable

--- chalets/urls.py
+++ chalets/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ChaletCreateView, ChaletListView
+from .views import ChaletCreateView, ChaletListView, ChaletUpdateView
 
 urlpatterns = [
     path('add/', ChaletCreateView.as_view(), name='chalet-add'),
     path('list/', ChaletListView.as_view(), name='chalet-list'),
+    path('update/<int:pk>/', ChaletUpdateView.as_view(), name='chalet-update'),
 ]
