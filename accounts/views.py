18ade8b0201011939bac953e0100515fd5769528
mohamedmostafa33 <mohamedabohamama15@gmail.com>
2025-07-14 17:05:48 +0300

    Enhance UserRegistrationView to return access and refresh JWT tokens after successful registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
+from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework import status, generics
-from .serializers import UserRegistrationSerializer
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import UserRegistrationSerializer
 
 class UserRegistrationView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
@@ -11,11 +12,16 @@ class UserRegistrationView(generics.CreateAPIView):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
+            refresh_token = RefreshToken.for_user(user)
+            access_token = refresh_token.access_token
+
             return Response({
                 'message': 'User registered successfully',
                 'user': {
                     'email': user.email,
                     'full_name': user.full_name
-                }
+                },
+                'refresh': str(refresh_token),
+                'access': str(access_token)
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
