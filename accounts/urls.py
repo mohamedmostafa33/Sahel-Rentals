355c651dd948ee795efd4c724b87d2a03fc5fffb
mohamedmostafa33 <mohamedabohamama15@gmail.com>
2025-07-16 13:48:58 +0300

    Define URL pattern for user login endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import UserRegistrationView
+from .views import UserRegistrationView, UserLoginView
 
 urlpatterns = [
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', UserRegistrationView.as_view(), name= 'user_register'),
+    path('register/', UserRegistrationView.as_view(), name='user_register'),
+    path('login/', UserLoginView.as_view(), name='user_login'),
 ]
