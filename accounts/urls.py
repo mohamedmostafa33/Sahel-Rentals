8581c17e735a69dca8b4a0587f71d6156e17857d
mohamedmostafa33 <mohamedabohamama15@gmail.com>
2025-07-16 15:57:29 +0300

    Add URL pattern for logout endpoint with blacklist implementation

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import UserRegistrationView, UserLoginView
+from .views import UserRegistrationView, UserLoginView, UserLogoutView
 
 urlpatterns = [
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', UserRegistrationView.as_view(), name='user_register'),
     path('login/', UserLoginView.as_view(), name='user_login'),
+    path('logout/', UserLogoutView.as_view(), name='user_logout'),
 ]
