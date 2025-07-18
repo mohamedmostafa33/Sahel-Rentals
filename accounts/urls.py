f541820318b53405b86874f193ae1a28c6e29712
mohamedmostafa33 <mohamedabohamama15@gmail.com>
2025-07-18 13:02:17 +0300

    Add URL pattern for reset password request endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import UserRegistrationView, UserLoginView, UserLogoutView
+from .views import (
+    UserRegistrationView, 
+    UserLoginView, 
+    UserLogoutView, 
+    ResetPasswordRequestView
+)
 
 urlpatterns = [
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', UserRegistrationView.as_view(), name='user_register'),
     path('login/', UserLoginView.as_view(), name='user_login'),
     path('logout/', UserLogoutView.as_view(), name='user_logout'),
+    path('reset-password/', ResetPasswordRequestView.as_view(), name='reset_password_request'),
 ]
