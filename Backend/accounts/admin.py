cc9912e53fb36ca75aeb368e172c114021d842bc
mohamedmostafa33 <mohamedabohamama15@gmail.com>
2025-08-02 17:44:45 +0300

    Make 'created_at' field readonly in CustomUserAdmin and remove it from 'Important dates' section

--- Backend/accounts/admin.py
+++ Backend/accounts/admin.py
@@ -7,13 +7,14 @@ class CustomUserAdmin(admin.ModelAdmin):
     search_fields = ('email', 'full_name', 'phone')
     list_filter = ('user_type', 'is_active', 'is_staff', 'created_at')
     ordering = ('-created_at',)
-    
+    readonly_fields = ('created_at',) 
+
     fieldsets = (
         (None, {'fields': ('email', 'full_name', 'phone', 'user_type', 'password')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'created_at')}),
+        ('Important dates', {'fields': ('last_login',)}),
     )
-    
+
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
